5847c237a425a10d1c3486c3d047caa474771cb3
Saule Westra <saule.westra@student.nhlstenden.com>
2023-03-14 11:01:15 +0100

    Kleine aanpassingen

--- main/main.ino
+++ main/main.ino
@@ -43,7 +43,7 @@ const int lineSensorFarLeft= A6;                          //
 const int lineSensorOuterLeft = A7;                       // 
 
 //===[Const int NeoPixels]==================================
-const int ledPin = 12;                                    // set Neopixel pin to 12
+const int ledPin = 12;                                    // -> NI
 const int ledCount = 4;                                   // Amount of Neopixels
 Adafruit_NeoPixel strip(ledCount, ledPin, NEO_GRB + NEO_KHZ800);
 
@@ -106,7 +106,7 @@ int getLineSensorSensitivity(int margin = 100){           // Get Sensitivity fro
 }
 
 /************************************
-***       Setup Digital I/O       ***
+***      Setup + Digital I/O      ***
 ************************************/
 
 void setup(){
@@ -135,9 +135,9 @@ void setup(){
   #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
     clock_prescale_set(clock_div_1);
   #endif
-    strip.begin();                                        // INITIALIZE NeoPixel strip object
+    strip.begin();                                        // Initialize NeoPixel strip
     strip.show();                                         // Turn OFF all pixels
-    strip.setBrightness(100);                             // Set BRIGHTNESS
+    strip.setBrightness(100);                             // Brightness (0-255)
 
   pinMode(ledPin, OUTPUT);                                // Set ledPin to OUTPUT
 
@@ -255,7 +255,7 @@ void rightLight(){
   strip.show();
 }
 
-//===[Elektromotoren + Line sensor]========================
+//===[Elektromotoren]========================
 void moveForward(int left, int right){
 
   goesForward = true;
@@ -291,6 +291,7 @@ void moveStop(){
   digitalWrite(leftMotorBackward, LOW);
 }
 
+//===[Line sensor]===========================
 void turnRight(){
   bool outerRight = analogRead(lineSensorOuterRight) > calibratedValue;
   bool farRight = analogRead(lineSensorFarRight) > calibratedValue;
